109c9da21b225dec51e08ea5ddadda1c44de0fd3
Scott Bowler <scott.bowler@learningfuze.com>
2020-07-23 00:33:14 +0000

    Completed add event

--- client/src/lib/date.js
+++ client/src/lib/date.js
@@ -7,3 +7,23 @@ export const days = [
   'Friday',
   'Saturday'
 ];
+
+export function buildTimeArray(step = 15) {
+  const times = [];
+  let hour = 0;
+  let minute = 0;
+  let connector = '0';
+  let time = '';
+  while (hour < 24) {
+    time = hour + connector + minute;
+    times.push(time);
+    minute += step;
+    connector = '';
+    if (minute >= 60) {
+      minute = 0;
+      hour++;
+      connector = '0';
+    }
+  }
+  return times;
+}
